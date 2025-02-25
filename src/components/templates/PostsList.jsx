9cd0f9d0923f120fe11618083807b89f321936bd
sahar_shakibafard <sahar.shakibafard.06@gmail.com>
2025-02-25 11:59:29 +0330

    The problem was not solved

--- src/components/templates/PostsList.jsx
+++ src/components/templates/PostsList.jsx
@@ -2,10 +2,11 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getPosts } from "services/user";
 import Loader from "../modules/Loader";
+import { sp } from "utils/number";
+
+import styles from "./PostsList.module.css";
 
 const PostsList = () => {
-  const baseURL = import.meta.env.VITE_API_URL;
-
   const { data, isPending } = useQuery({
     queryKey: ["get-posts-list"],
     queryFn: getPosts,
@@ -14,26 +15,30 @@ const PostsList = () => {
 
   return (
     <>
-      {isPending ? (
-        <Loader />
-      ) : (
-        <>
-          <h3>آگهی های شما</h3>
-          {data.data.posts.map((post) => (
-            <div key={post._id}>
-              <img src={`${baseURL}${post.images[0]}`} />
-              <div>
-                <p>{post.options.title}</p>
-                <span>{post.options.content}</span>
+      <div className={styles.list}>
+        {isPending ? (
+          <Loader />
+        ) : (
+          <>
+            <h3>آگهی های شما</h3>
+            {data?.data.posts.map((post) => (
+              <div key={post._id} className={styles.post}>
+                <img
+                  src={`${import.meta.env.VITE_API_URL}${post.images[0]}`}
+                />
+                <div>
+                  <p>{post.options?.title}</p>
+                  <span>{post.options?.content}</span>
+                </div>
+                <div className={styles.price}>
+                  <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                  <span>{sp(post.amount)} تومان</span>
+                </div>
               </div>
-              <div>
-                <p>{post.createdAt}</p>
-                <span>{post.amount} تومان</span>
-              </div>
-            </div>
-          ))}
-        </>
-      )}
+            ))}
+          </>
+        )}
+      </div>
     </>
   );
 };
